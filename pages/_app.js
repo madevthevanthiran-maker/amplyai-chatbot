1648bb9b0bf90d9200c9c8eb32cede137ff0838d
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-08-30 00:58:51 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import "../styles/globals.css";
+// pages/_app.js
+import Head from "next/head";
+import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head><title>Progress Partner</title></Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp;
