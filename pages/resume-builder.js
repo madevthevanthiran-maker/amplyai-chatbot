e7cf4a9dbf837ebc1f68229a495c74cdccbf0674
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-08-30 00:58:20 +0800

    Update resume-builder.js

--- pages/resume-builder.js
+++ pages/resume-builder.js
@@ -1,2 +1,5 @@
 // pages/resume-builder.js
-export { default } from "./hire-helper";
+export default function ResumeBuilderRedirect() {
+  if (typeof window !== "undefined") window.location.replace("/hire-helper");
+  return null;
+}
