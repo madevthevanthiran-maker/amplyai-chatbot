ee07a6f6a20f2a85b6d34e0a4d97b17249d2f09c
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-08-30 14:24:54 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 // pages/_app.js
 import Head from "next/head";
-import "@/styles/globals.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head><title>Progress Partner</title></Head>
+      <Head>
+        <title>AmplyAI — Progress Partner</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
