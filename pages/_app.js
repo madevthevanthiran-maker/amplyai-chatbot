63ccdea4da65558202b1af5884762a1070ef6305
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-08-30 17:05:59 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import Head from "next/head";
-import "../styles/globals.css";
+import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,7 +10,9 @@ export default function App({ Component, pageProps }) {
         <title>AmplyAI — Progress Partner</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
