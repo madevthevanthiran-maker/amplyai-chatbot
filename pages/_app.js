c88f8dbc2fb05a8b86a904ec27fab5b98ad772ac
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-01 00:18:54 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 // pages/_app.js
--import { Analytics } from '@vercel/analytics/react';
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head><title>Progress Partner</title></Head>
+      <Head>
+        <title>Progress Partner</title>
+      </Head>
       <Component {...pageProps} />
--     <Analytics />
+      <Analytics />
     </>
   );
 }
