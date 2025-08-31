e95ab0a69ac653b8677f280405861b8f087018e9
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-01 00:33:16 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
 import Head from "next/head";
-import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
@@ -8,9 +7,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Progress Partner</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
