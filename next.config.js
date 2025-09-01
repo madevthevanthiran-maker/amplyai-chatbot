9fed1a881cc402f3f8deb7dfba33a9297ce40fb2
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-01 21:51:38 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // ✅ Do not run ESLint during `next build` on Vercel
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
