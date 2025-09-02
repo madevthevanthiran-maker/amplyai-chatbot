ab7cb19b6502d011531ce76256bcbf579a2ce733
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-02 14:42:36 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      borderRadius: { "2xl": "1rem" }
+    },
   },
-  plugins: []
+  plugins: [],
 };
