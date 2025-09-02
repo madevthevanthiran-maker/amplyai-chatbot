0cb816b9c43a3c2061a1d6d34a03d64fe8554fad
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-02 14:12:58 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,12 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Progress Partner</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Plausible */}
+        <script
+          defer
+          data-domain="amplyai.org"
+          src="https://plausible.io/js/script.js"
+        />
       </Head>
       <Component {...pageProps} />
     </>
