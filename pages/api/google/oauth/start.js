f54237d1b320ea178b378a5a01ce9f329c1c6219
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-10 16:45:03 +0800

    Update start.js

--- pages/api/google/oauth/start.js
+++ pages/api/google/oauth/start.js
@@ -1,9 +1,9 @@
 // /pages/api/google/oauth/start.js
 import { getAuthUrl } from "../../../../lib/googleClient";
 
-export default async function handler(req, res) {
-  const returnTo = req.query.returnTo || "/settings";
-  const url = getAuthUrl(returnTo);
-  res.writeHead(302, { Location: url });
-  res.end();
+export default function handler(req, res) {
+  const state = typeof req.query.state === "string" ? req.query.state : "/settings";
+  const url = getAuthUrl(state);
+  res.setHeader("Cache-Control", "no-store");
+  res.redirect(302, url);
 }
