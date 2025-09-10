72f74658f169aa4dbde3152cfb67d61b3952a451
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-10 19:50:00 +0800

    Update start.js

--- pages/api/google/oauth/start.js
+++ pages/api/google/oauth/start.js
@@ -1,10 +1,9 @@
 // /pages/api/google/oauth/start.js
-import { getAuthUrl } from "../../../lib/googleClient";
+import { getAuthUrl } from "../../../../lib/googleClient";
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const state = typeof req.query.state === "string" ? req.query.state : "/settings";
   const url = getAuthUrl(state);
-  // Always redirect to PROD auth URL (because getAuthUrl uses APP_URL)
   res.writeHead(302, { Location: url });
   res.end();
 }
