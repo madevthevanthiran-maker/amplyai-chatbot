8e64517580d0fcb0f2db529584a60296b8e21be8
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-11 13:55:59 +0800

    Update start.js

--- pages/api/google/oauth/start.js
+++ pages/api/google/oauth/start.js
@@ -1,9 +1,10 @@
 // /pages/api/google/oauth/start.js
 import { getAuthUrl } from "../../../../lib/googleClient";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const state = typeof req.query.state === "string" ? req.query.state : "/settings";
   const url = getAuthUrl(state);
+  // Use a 302 to the consent screen
   res.writeHead(302, { Location: url });
   res.end();
 }
