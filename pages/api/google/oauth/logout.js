23539d4a07b7c7e947ba5b1781f6012643a147f0
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-12 00:47:09 +0800

    Update logout.js

--- pages/api/google/oauth/logout.js
+++ pages/api/google/oauth/logout.js
@@ -1,6 +1,12 @@
-import { clearSession } from "../../../../lib/session";
-
-export default function handler(req, res) {
-  clearSession(res);
-  res.redirect(302, "/settings");
+// /pages/api/google/oauth/logout.js
+export default function handler(_req, res) {
+  // Clear cookie
+  res.setHeader(
+    "Set-Cookie",
+    `gcal_tok=; Path=/; HttpOnly; Max-Age=0; SameSite=Lax; ${
+      process.env.NODE_ENV !== "development" ? "Secure;" : ""
+    }`
+  );
+  res.writeHead(302, { Location: "/settings" });
+  res.end();
 }
