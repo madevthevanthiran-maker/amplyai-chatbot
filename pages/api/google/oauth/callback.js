0320667299291bdce4433fc99673da650e281017
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-12 00:13:46 +0800

    Update callback.js

--- pages/api/google/oauth/callback.js
+++ pages/api/google/oauth/callback.js
@@ -1,71 +1,27 @@
-// /pages/api/google/oauth/callback.js
-import { exchangeCodeForTokens, setTokenCookies } from "../../../../lib/googleClient";
-
-function htmlEscape(s = "") {
-  return String(s)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;");
-}
+import { getOAuthClient } from "../../../../lib/googleClient";
+import { setSession } from "../../../../lib/session";
 
 export default async function handler(req, res) {
+  const { code, state } = req.query;
+
+  if (!code) {
+    res.status(400).send("OAuth failed. Try again from Settings.");
+    return;
+  }
+
   try {
-    const { code, state, error, error_description, ...rest } = req.query || {};
-
-    // If Google returned an error param, show it plainly.
-    if (error) {
-      console.error("[oauth/callback] provider error:", { error, error_description, rest });
-      return res
-        .status(400)
-        .send(
-          `OAuth error from Google:<br><pre>${htmlEscape(
-            JSON.stringify({ error, error_description, rest }, null, 2)
-          )}</pre>`
-        );
-    }
-
-    if (!code) {
-      console.error("[oauth/callback] missing code. query=", req.query);
-      return res
-        .status(400)
-        .send(
-          `OAuth failed: "code" missing.<br><pre>${htmlEscape(
-            JSON.stringify(req.query, null, 2)
-          )}</pre>`
-        );
-    }
-
-    try {
-      const { tokens } = await exchangeCodeForTokens(code);
-      setTokenCookies(res, tokens);
-      const redirectTo = state && typeof state === "string" ? state : "/settings";
-      return res.redirect(302, redirectTo);
-    } catch (err) {
-      console.error("[oauth/callback] token exchange failed:", {
-        message: err?.message,
-        code: err?.code,
-        response: err?.response?.data,
-      });
-      // Show full diagnostic so you can fix quickly (redirect mismatch etc.)
-      return res
-        .status(400)
-        .send(
-          `Token exchange failed.<br><b>Most common cause:</b> Redirect URI mismatch.<br><br><pre>${htmlEscape(
-            JSON.stringify(
-              {
-                message: err?.message,
-                code: err?.code,
-                response: err?.response?.data,
-                query: req.query,
-              },
-              null,
-              2
-            )
-          )}</pre>`
-        );
-    }
-  } catch (outer) {
-    console.error("[oauth/callback] outer error:", outer?.message);
-    return res.status(400).send("OAuth failed. Try again from Settings.");
+    const client = getOAuthClient();
+    const { tokens } = await client.getToken(code.toString());
+    setSession(res, tokens);
+    res.redirect(302, state ? state.toString() : "/settings");
+  } catch (e) {
+    // Helpful page if Google bounces you to “redirect_uri_mismatch”
+    res.status(400).send(
+      `Token exchange failed.<br><b>Most common cause:</b> Redirect URI mismatch.<br><br><pre>${JSON.stringify(
+        { message: e.message, query: req.query },
+        null,
+        2
+      )}</pre>`
+    );
   }
 }
