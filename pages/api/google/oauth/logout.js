f1c0bea532597823aaa0aae218f2fd2dc24e8434
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-14 17:55:05 +0800

    Update logout.js

--- pages/api/google/oauth/logout.js
+++ pages/api/google/oauth/logout.js
@@ -1,9 +1,7 @@
-// /pages/api/google/oauth/logout.js
-import { clearGoogleTokens } from "@/lib/googleCookie";
+import { clearTokensCookie } from "../../../../lib/googleCookie";
 
 export default async function handler(req, res) {
-  clearGoogleTokens(res);
-  const to = typeof req.query.returnTo === "string" ? req.query.returnTo : "/settings";
-  const sep = to.includes("?") ? "&" : "?";
-  return res.redirect(`${to}${sep}loggedOut=1`);
+  clearTokensCookie(res, req);
+  const { returnTo = "/settings" } = req.query;
+  res.redirect(returnTo);
 }
