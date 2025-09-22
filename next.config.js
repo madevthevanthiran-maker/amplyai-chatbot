5af74b1234d40e2014313e9e5ebf6276324db59b
madevthevanthiran-maker <madev.thevanthiran@outlook.com>
2025-09-22 11:44:17 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Ensure nothing rewrites /api/**
-  async rewrites() { return []; },
-  async redirects() { return []; },
+
+  // Keep rewrites/redirects EMPTY or make sure they NEVER match /api/**
+  async rewrites() {
+    return [];
+  },
+  async redirects() {
+    return [];
+  },
 };
 
 module.exports = nextConfig;
